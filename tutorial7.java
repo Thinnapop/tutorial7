a1ec7bf20a3648df3fb56f12f880c76e271127da
DiceX <149762497+Thinnapop@users.noreply.github.com>
2025-02-23 20:54:15 +0700

    Create tutorial7.java

---
+++ tutorial7.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Graph {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjList = new LinkedList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        adjList[src].add(dest);
+        adjList[dest].add(src);
+    }
+
+    public void printGraph() {
+        System.out.println("Adjacency List of the Graph:");
+        for (int i = 0; i < vertices; i++) {
+            System.out.print(i + " -> ");
+            for (int neighbor : adjList[i]) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        
+        graph.printGraph();
+    }
+}
